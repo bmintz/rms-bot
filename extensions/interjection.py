bbafb1ca6f05723b85a09e555cb3abf7c17e00be
bmintz <bmintz@protonmail.com>
2018-08-30 21:50:13 +0000

    extensions/interjection.check: allow messages without GNU or Linux lol

--- extensions/interjection.py
+++ extensions/interjection.py
@@ -5,7 +5,15 @@ from discord.ext import commands
 gnu_re = re.compile(r'(?P<GNU>GNU)?(?:.?|\s*)(?P<Linux>Linux)', re.IGNORECASE)
 
 def check(text):
-	return all(not match['GNU'] for match in gnu_re.finditer(text))
+	"""return whether the message contains an occurence of "Linux" without "GNU""""
+	iterated = False
+	for match in gnu_re.finditer(text):
+		iterated = True
+		if match['GNU']:
+			return False
+	# don't return True if the message contains neither GNU nor Linux
+	# (ie, finditer was empty)
+	return iterated
 
 class Interjection:
 	def __init__(self, bot, interjection):
